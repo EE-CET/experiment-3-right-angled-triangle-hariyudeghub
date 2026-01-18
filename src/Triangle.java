439c647a5113b9a1558d808eedde6ad5a42ba29b
Harikrishna <harikrishnas9a@gmail.com>
2026-01-18 07:40:29 +0530

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,9 +1,10 @@
 public class Triangle {
-    public static void main(String args[]){
-        for(int i=0;i<3;i++){
-            for(int j=0;j<2*i+1;j++){
-                System.out.print("* ");
-                if(j<2*i){
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+                if (j < 2 * i) {
                     System.out.print(" ");
                 }
             }
